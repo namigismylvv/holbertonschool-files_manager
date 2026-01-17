b2e01e5f87126249bd7cf9edb84273cd096a59db
namigismylvv <11869@holbertonstudents.com>
2026-01-18 02:07:09 +0400

    feat

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,46 +1,75 @@
 import { v4 as uuidv4 } from 'uuid';
 import sha1 from 'sha1';
 import redisClient from '../utils/redis';
-import dbClient from '../utils/db';
-import getIdAndKey from '../utils/users';
+import userUtils from '../utils/user';
 
 class AuthController {
-  static async getConnect(req, res) {
-    const Authorization = req.header('Authorization') || '';
+  /**
+   * Should sign-in the user by generating a new authentication token
+   *
+   * By using the header Authorization and the technique of the Basic auth
+   * (Base64 of the <email>:<password>), find the user associate to this email
+   * and with this password (reminder: we are storing the SHA1 of the password)
+   * If no user has been found, return an error Unauthorized with a status code 401
+   * Otherwise:
+   * Generate a random string (using uuidv4) as token
+   * Create a key: auth_<token>
+   * Use this key for storing in Redis (by using the redisClient create previously)
+   * the user ID for 24 hours
+   * Return this token: { "token": "155342df-2399-41da-9e8c-458b6ac52a0c" }
+   * with a status code 200
+   */
+  static async getConnect(request, response) {
+    const Authorization = request.header('Authorization') || '';
 
-    const creds = Authorization.split(' ')[1];
-    if (!creds) return res.status(401).send({ error: 'Unauthorized' });
+    const credentials = Authorization.split(' ')[1];
 
-    const decodedCreds = Buffer.from(creds, 'base64').toString('utf-8');
+    if (!credentials)
+      return response.status(401).send({ error: 'Unauthorized' });
 
-    const [email, pass] = decodedCreds.split(':');
-    if (!email || !pass) return res.status(401).send({ error: 'Unauthorized' });
+    const decodedCredentials = Buffer.from(credentials, 'base64').toString(
+      'utf-8'
+    );
 
-    const secPass = sha1(pass);
+    const [email, password] = decodedCredentials.split(':');
 
-    const user = await dbClient.users.findOne({
+    if (!email || !password)
+      return response.status(401).send({ error: 'Unauthorized' });
+
+    const sha1Password = sha1(password);
+
+    const user = await userUtils.getUser({
       email,
-      password: secPass,
+      password: sha1Password,
     });
-    if (!user) return res.status(401).send({ error: 'Unauthorized' });
+
+    if (!user) return response.status(401).send({ error: 'Unauthorized' });
 
     const token = uuidv4();
     const key = `auth_${token}`;
-    const expiration = 24 * 3600;
+    const hoursForExpiration = 24;
 
-    await redisClient.set(key, user._id.toString(), expiration);
+    await redisClient.set(key, user._id.toString(), hoursForExpiration * 3600);
 
-    return res.status(200).send({ token });
+    return response.status(200).send({ token });
   }
 
-  static async getDisconnect(req, res) {
-    const { userId, key } = await getIdAndKey(req);
+  /**
+   * Should sign-out the user based on the token
+   *
+   * Retrieve the user based on the token:
+   * If not found, return an error Unauthorized with a status code 401
+   * Otherwise, delete the token in Redis and return nothing with a
+   * status code 204
+   */
+  static async getDisconnect(request, response) {
+    const { userId, key } = await userUtils.getUserIdAndKey(request);
 
-    if (!userId) return res.status(401).send({ error: 'Unauthorized' });
+    if (!userId) return response.status(401).send({ error: 'Unauthorized' });
 
     await redisClient.del(key);
 
-    return res.status(204).send();
+    return response.status(204).send();
   }
 }
 
--- utils/db.js
+++ utils/db.js
@@ -5,32 +5,48 @@ const DB_PORT = process.env.DB_PORT || 27017;
 const DB_DATABASE = process.env.DB_DATABASE || 'files_manager';
 const url = `mongodb://${DB_HOST}:${DB_PORT}`;
 
+/**
+ * Class for performing operations with Mongo service
+ */
 class DBClient {
   constructor() {
-    MongoClient.connect(url, { useUnifiedTopology: true }, (error, client) => {
-      if (!error) {
+    MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
+      if (!err) {
+        // console.log('Connected successfully to server');
         this.db = client.db(DB_DATABASE);
-        this.users = this.db.collection('users');
-        this.files = this.db.collection('files');
+        this.usersCollection = this.db.collection('users');
+        this.filesCollection = this.db.collection('files');
       } else {
-        console.log(error.message);
+        console.log(err.message);
         this.db = false;
       }
     });
   }
 
+  /**
+   * Checks if connection to Redis is Alive
+   * @return {boolean} true if connection alive or false if not
+   */
   isAlive() {
     return !!this.db;
   }
 
+  /**
+   * Returns the number of documents in the collection users
+   * @return {number} amount of users
+   */
   async nbUsers() {
-    const userCount = this.users.countDocuments();
-    return userCount;
+    const numberOfUsers = this.usersCollection.countDocuments();
+    return numberOfUsers;
   }
 
+  /**
+   * Returns the number of documents in the collection files
+   * @return {number} amount of files
+   */
   async nbFiles() {
-    const fileCount = this.files.countDocuments();
-    return fileCount;
+    const numberOfFiles = this.filesCollection.countDocuments();
+    return numberOfFiles;
   }
 }
 
