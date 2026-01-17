a71249280d802f6881d8201b9b3fd3c3d5036b88
namigismylvv <11869@holbertonstudents.com>
2026-01-18 02:29:41 +0400

    fix

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const express = require("express");
-const router = require("./routes/index");
+const express = require('express');
+const router = require('./routes/index');
 
 const app = express();
 const port = process.env.PORT || 5000;
